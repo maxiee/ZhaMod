5c719a19da788417cb7cc279cd18c9ccbb3b23a6
Maxiee <maxieewong@gmail.com>
2015-08-04 18:35:09 +0800

    fix xposed_init location

--- app/src/main/java/com/maxiee/zhamod/WeiboHook.java
+++ app/src/main/java/com/maxiee/zhamod/WeiboHook.java
@@ -1,7 +1,18 @@
 package com.maxiee.zhamod;
 
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
 /**
  * Created by maxiee on 15-8-4.
  */
-public class WeiboHook {
+public class WeiboHook implements IXposedHookLoadPackage{
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (!loadPackageParam.packageName.equals("com.sina.weibo")) {
+            return;
+        }
+        XposedBridge.log("主人,发现渣浪客户端!");
+    }
 }
