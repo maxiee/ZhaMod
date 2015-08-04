c05d44d99bedadb78d89c1eb06e13986a890342f
Maxiee <maxieewong@gmail.com>
2015-08-04 21:53:25 +0800

    WeiboHook.java: change tab text.

--- app/src/main/java/com/maxiee/zhamod/WeiboHook.java
+++ app/src/main/java/com/maxiee/zhamod/WeiboHook.java
@@ -1,7 +1,12 @@
 package com.maxiee.zhamod;
 
+import android.view.View;
+import android.widget.TextView;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 /**
@@ -14,5 +19,25 @@ public class WeiboHook implements IXposedHookLoadPackage{
             return;
         }
         XposedBridge.log("主人,发现渣浪客户端!");
+
+        final Class<?> homeActivity = XposedHelpers.findClass("com.sina.weibo.view.TabView", loadPackageParam.classLoader);
+
+        XposedHelpers.findAndHookMethod(
+                homeActivity,
+                "setText",
+                String.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        String text = (String) XposedHelpers.getObjectField(param.thisObject, "f");
+                        XposedBridge.log(text);
+                        XposedHelpers.setObjectField(param.thisObject, "f", "渣");
+                        ((View) param.thisObject).invalidate();
+                    }
+                });
     }
 }
