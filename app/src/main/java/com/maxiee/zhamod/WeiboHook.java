715af63bce95bcb87de7b71ff67bd75dd0a4bb7d
Maxiee <maxieewong@gmail.com>
2015-08-05 15:09:05 +0800

    WeiboHook.java: change compose button image.

--- app/src/main/java/com/maxiee/zhamod/WeiboHook.java
+++ app/src/main/java/com/maxiee/zhamod/WeiboHook.java
@@ -1,29 +1,44 @@
 package com.maxiee.zhamod;
 
+import android.content.res.XModuleResources;
+import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 /**
  * Created by maxiee on 15-8-4.
  */
-public class WeiboHook implements IXposedHookLoadPackage{
+public class WeiboHook implements IXposedHookLoadPackage, IXposedHookInitPackageResources, IXposedHookZygoteInit{
+    private static String MODULE_PATH = null;
+    private static final String PACKAGE_NAME = "com.sina.weibo";
+    private XModuleResources mModRes;
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        MODULE_PATH = startupParam.modulePath;
+    }
+
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        if (!loadPackageParam.packageName.equals("com.sina.weibo")) {
+        if (!loadPackageParam.packageName.equals(PACKAGE_NAME)) {
             return;
         }
         XposedBridge.log("主人,发现渣浪客户端!");
 
-        final Class<?> homeActivity = XposedHelpers.findClass("com.sina.weibo.view.TabView", loadPackageParam.classLoader);
+        final Class<?> tabView = XposedHelpers.findClass("com.sina.weibo.view.TabView", loadPackageParam.classLoader);
 
         XposedHelpers.findAndHookMethod(
-                homeActivity,
+                tabView,
                 "setText",
                 String.class,
                 new XC_MethodHook() {
@@ -39,5 +54,30 @@ public class WeiboHook implements IXposedHookLoadPackage{
                         ((View) param.thisObject).invalidate();
                     }
                 });
+
+        final Class<?> mainTabActivity = XposedHelpers.findClass("com.sina.weibo.MainTabActivity", loadPackageParam.classLoader);
+
+        XposedHelpers.findAndHookMethod(
+                mainTabActivity,
+                "onCreate",
+                Bundle.class,
+                new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                        RelativeLayout relativeLayout = (RelativeLayout) XposedHelpers.getObjectField(param.thisObject, "U");
+                        ImageView composeImage = (ImageView) relativeLayout.getChildAt(0);
+                        composeImage.clearColorFilter();
+                        composeImage.setImageDrawable(mModRes.getDrawable(R.drawable.bei));
+                    }
+                }
+        );
+    }
+
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam initPackageResourcesParam) throws Throwable {
+        if (!initPackageResourcesParam.packageName.equals(PACKAGE_NAME)) {
+            return;
+        }
+        mModRes= XModuleResources.createInstance(MODULE_PATH, initPackageResourcesParam.res);
     }
 }
