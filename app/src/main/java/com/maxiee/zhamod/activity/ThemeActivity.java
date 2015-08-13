7b933bd60bbb2fe9c8dc3ac7fd88c9cf2c614d50
Maxiee <maxieewong@gmail.com>
2015-08-13 14:55:39 +0800

    ThemeActivity.java: store selected background to SP. use toolbar.

--- app/src/main/java/com/maxiee/zhamod/activity/ThemeActivity.java
+++ app/src/main/java/com/maxiee/zhamod/activity/ThemeActivity.java
@@ -1,56 +1,59 @@
 package com.maxiee.zhamod.activity;
 
-import android.graphics.Color;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 
+import com.maxiee.zhamod.Constants;
 import com.maxiee.zhamod.R;
 
 /**
  * Created by maxiee on 15-8-7.
  */
 public class ThemeActivity extends AppCompatActivity implements View.OnClickListener{
-    private static final int[] COLORS = {
-            R.color.red,
-            R.color.pink,
-            R.color.purple,
-            R.color.indigo,
-            R.color.blue,
-            R.color.teal,
-            R.color.lime,
-            R.color.yellow,
-            R.color.orange,
-            R.color.gray
-    };
 
-    private static final int ROWS = 3;
-    private static final int COLUMNS = 4;
+    private static final int ROWS = 4;
+    private static final int COLUMNS = 3;
 
+    private Toolbar mToolbar;
     private GridLayout mGrid;
+    private View mCurrentSelect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_theme);
 
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         mGrid = (GridLayout) findViewById(R.id.grid);
+        mCurrentSelect = (View) findViewById(R.id.current_select);
 
         generateColoredButtons();
+
+        int spCurrent = getSharedPreferences(Constants.SP_FILE, Context.MODE_WORLD_READABLE).getInt(Constants.SP_BACKGROUND, 0);
+        mCurrentSelect.setBackgroundColor(getResources().getColor(Constants.COLORS[spCurrent]));
     }
 
     private void generateColoredButtons() {
         for (int i=0; i<ROWS; i++) {
             for (int j=0; j<COLUMNS; j++) {
                 int count = COLUMNS * i + j;
-                if (count > COLORS.length - 1) {
+                if (count > Constants.COLORS.length - 1) {
                     return;
                 }
                 Button b = new Button(this);
                 b.setBackgroundResource(R.drawable.round_button);
-                b.setBackgroundColor(getResources().getColor(COLORS[count]));
+                b.setBackgroundColor(getResources().getColor(Constants.COLORS[count]));
+                b.setTag(count);
                 b.setOnClickListener(this);
                 GridLayout.LayoutParams params = new GridLayout.LayoutParams();
                 params.height = GridLayout.LayoutParams.WRAP_CONTENT;
@@ -65,6 +68,20 @@ public class ThemeActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+        int count = (int) v.getTag();
+        mCurrentSelect.setBackgroundColor(getResources().getColor(Constants.COLORS[count]));
+        SharedPreferences.Editor editor = getSharedPreferences(Constants.SP_FILE, Context.MODE_WORLD_READABLE).edit();
+        editor.putInt(Constants.SP_BACKGROUND, count);
+        editor.apply();
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            this.onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
