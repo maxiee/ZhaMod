8049d36497e9e585046c49bab85fb289947ac9aa
Maxiee <maxieewong@gmail.com>
2015-08-13 14:54:45 +0800

    MainActivity.java: use Toolbar.

--- app/src/main/java/com/maxiee/zhamod/activity/MainActivity.java
+++ app/src/main/java/com/maxiee/zhamod/activity/MainActivity.java
@@ -1,19 +1,25 @@
 package com.maxiee.zhamod.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.maxiee.zhamod.MainSettingFragment;
 import com.maxiee.zhamod.R;
+import com.maxiee.zhamod.activity.fragment.MainSettingFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
         getFragmentManager()
                 .beginTransaction()
