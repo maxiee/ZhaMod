1f2b0ba6a8847eca237b2e26f5732702a474181c
Maxiee <maxieewong@gmail.com>
2015-08-13 14:53:49 +0800

    Constants.java: store constants.

---
+++ app/src/main/java/com/maxiee/zhamod/Constants.java
@@ -0,0 +1,21 @@
+package com.maxiee.zhamod;
+
+/**
+ * Created by maxiee on 15-8-13.
+ */
+public class Constants {
+    public static final String SP_FILE = "zhamod";
+    public static final String SP_BACKGROUND = "background";
+    public static final int[] COLORS = {
+            R.color.red,
+            R.color.pink,
+            R.color.purple,
+            R.color.indigo,
+            R.color.blue,
+            R.color.teal,
+            R.color.lime,
+            R.color.yellow,
+            R.color.orange,
+            R.color.gray
+    };
+}
